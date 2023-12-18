3cec932a5dce05ee26914774378b301ae0ae3522
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2023-12-18 22:15:25 +0100

    updated dependencis

--- client/components/Application/Authenticator.jsx
+++ client/components/Application/Authenticator.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import LoginCallback from "../Authentication/OpenIdConnect/Callback/LoginCallback";
 import LoginPage from "./LoginPage";
 import Application from "./Application";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const Authenticator = () => {
   async function loadConfig() {
